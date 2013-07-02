c255a680d17296fe51b692041077a0e99dafc696
federicolucca <federico@federico-Precision-M4700>
2013-07-02 14:53:14 +0200

    init

---
+++ src/main/java/eu/trentorise/smartcampus/universiadi/controller/EventoController.java
@@ -0,0 +1,10 @@
+package eu.trentorise.smartcampus.universiadi.controller;
+
+import org.springframework.stereotype.Controller;
+
+
+
+@Controller("eventoController")
+public class EventoController {
+
+}
